3fb9fabbc6ed3fadfe8e9c543e7975dc1eda331c
NIGAM YADAV <71872897+nigamyadav72@users.noreply.github.com>
2025-11-19 08:45:40 +0545

    control samma

---
+++ control.dart
@@ -0,0 +1,58 @@
+// bool isAllowed = false;
+void main() {
+  //if else statements
+
+//   int age = 18;
+
+//   if (age >= 21) {
+//     print('ADULT 21');
+//   } else if (age >= 18) {
+//     print('ADULT');
+//   } else {
+//     print('CHILD');
+//   }
+
+//   if (age != 18 && isAllowed) {
+//     print('haha');
+//   } else {
+//     print('naha');
+//   }
+
+//   String someValue = 'Hi';
+//   if (someValue.isEmpty) {
+//     print('wow');
+//   } else {
+//     print('naha');
+//   }
+
+//   //ternary operator
+
+//   String value = someValue.startsWith('H') ? 'wow' : 'naha';
+//   print(value);
+
+//   // switch statement
+
+// switch (someValue) {
+//   case 'Hi' when age>=18:
+//     print('Hello');
+//     // break; it is not necessary to put break here in dart , we can use only when empty cases like no print inside case 
+//     case 'Hi!':
+//     print('Hello!');
+//     case 'Hi!!':
+//     print('Hello!!');
+//     case 'Hi!!!':
+//     print('Hello!!!');
+//   default: 
+//     print('no match');
+  
+// }
+
+//Loops!
+
+
+
+
+
+
+}
+
--- variable.dart
+++ variable.dart
@@ -1,27 +1,28 @@
+String? someValue;
 void main() {
   //variables
   //<datatype> <variablename> = value;
   //first value
-  int firstValue = 302;
-  print(firstValue);
+  // int firstValue = 302;
+  // print(firstValue);
 
   //second value
-  int secondValue = 335;
-  print(secondValue.isEven);
-  double test = 335.5;
-  print(test);
+  // int secondValue = 335;
+  // print(secondValue.isEven);
+  // double test = 335.5;
+  // print(test);
 
-  bool isAdult = false;
+  // bool isAdult = false;
 
-  print(isAdult);
+  // print(isAdult);
 
   //dynamic can take any value but it is not recommended to use dynamic
   // . use garera attribute haru use garna milxa like secondValue.isEven
 
-  String somevalue = "10";
-  print(somevalue.isEmpty);
-  print(somevalue);
-  print(somevalue.length);
+  // String somevalue = "10";
+  // print(somevalue.isEmpty);
+  // print(somevalue);
+  // print(somevalue.length);
 
   // dynamic hello = '123123124';
   // print(hello + 3); // it will give compile time error
@@ -29,7 +30,67 @@ void main() {
   //how to change variable name
   // dart is case sensative
 
-  int first = 20;
-  first = 100;
-  print(first);
+  // int first = 20;
+  // print(first);
+  // first = first + 30; // first += 30 is also same
+  // print(first);
+
+  // String greeting = 'Hello, world';
+  // print(greeting);
+  // greeting = greeting + ' yooo ';
+  // greeting =
+  //     '${greeting}  yooo '; //here dollar is used to do dynamic change in string
+  // greeting = '${greeting.length}  yooo ';
+
+  // print(greeting);
+
+  // greeting =
+  //     '\$10'; //yeha hamle backslash laga xam taki dollar ko importance harauxa ani normal letter jasto behave garos ra dollar print hos output ma
+
+  // print(greeting);
+
+  // greeting = '''hello
+  // world'''; // do multiline string like hello in one line and world in one
+  // print(greeting);
+
+  // greeting = 'hello \nworld';
+  // print(greeting);
+
+  // // var/final/const variableName = value
+
+  // var check = '10';
+  // final check1 = '10';
+  // const check2 = '10';
+  // print(check);
+  // print(check1);
+  // print(check2);
+
+  // check = '1000';
+  // check1 = '1000';
+  // check2 = '1000';
+  // print(check);
+  // print(check1);
+  // print(check2);
+
+  final someValue2 = DateTime.now();
+  // const someValue3 = DateTime.now(); this will give a runtime error
+  const someValue3 = 10;
+
+  print(someValue2);
+  print(someValue3);
+
+  //optional variables or nullable variables
+  //string/int/bool and null
+  // final somevalue = null;  it is not preffered since it make it dynamic
+  // String? someValue;
+
+  print(someValue);
+
+  someValue = '345 hello world';
+
+  print(someValue?.length??0);// here ??0 means if it is null then print lenght 0 otherwise just print the length of the string 
+
+  someValue = null;
+
+  print(someValue?.length??0); //? it means if it is null then print null and if it is not null then use .length
 }

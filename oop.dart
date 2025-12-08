7e3bf700d6be440fcd7d494e4f9054bfb38436ab
NIGAM YADAV <71872897+nigamyadav72@users.noreply.github.com>
2025-12-08 22:15:30 +0545

    oop.dart is updated

--- oop.dart
+++ oop.dart
@@ -1,16 +1,74 @@
-void main() {
-  //object oriented programming (OOP)
-  // 1. Polymorphism
-  // 2. Abstraction
-  // 3. Inheritance
-  // 4. Encapsulation
+// void main() {
+//   //object oriented programming (OOP)
+//   // 1. Polymorphism
 
-  
-}
+//   Animal cat = Cat();
+//   cat.sound();
+//   cat = Dog();
+//   cat.sound();
+
+//   // 2. Abstraction
+
+// }
+
+// class Animal {
+//   void sound() {
+//     print('Animal making sound');
+//   }
+// }
+
+// class Cat extends Animal {
+//   @override
+//   void sound() {
+//     print('cat making sound');
+//   }
+// }
+
+// class Dog extends Animal {
+//   @override
+//   void sound() {
+//     print('Dog making sound');
+//   }
+// }
+
+// void main() {
+//   //object oriented programming (OOP)
+
+//   // 2. Abstraction
+//   Animal anim1 = Cat();
+//   anim1.sound();
+//   Animal anim2 = Dog();
+//   anim2.sound();
+
+// }
+
+// abstract class Animal {
+//   void sound();
+// }
+
+// class Cat extends Animal {
+//   @override
+//   void sound() {
+//     print('cat making sound');
+//   }
+// }
+
+// class Dog extends Animal {
+//   @override
+//   void sound() {
+//     print('Dog making sound');
+//   }
+// }
+
+// void main() {}
+
+// class Person {
+//   String _name; // this provides data protection which means encapsulation
+
+//   void _getName(){
+
+//   }
+
+// }
 
 
-class Animal {
-    void sound(){
-      print('Animal making sound');
-    }
-  }
